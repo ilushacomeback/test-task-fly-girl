f6e8bd2cba6f4e9b13e834cb1960d2986b699b35
ilushacomeback <ilya.rudenko.2105@mail.ru>
2024-10-25 16:37:48 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 (function init() {
-    const girl = document.querySelector('.girl')
+  const girl = document.querySelector('.girl');
+  const hole = document.querySelector('.black-hole');
 
-    girl.addEventListener('animationend', () => {
-        girl.style.bottom = '20px'
-        girl.style.animation = 'levitate 8s ease 0s infinite'
-    })
-})()
+  const animationHole = (e) => {
+    if (e.animationName === 'holeBig') {
+      girl.style.animation = 'girl 3s';
+    } else {
+      hole.removeEventListener('animationend', animationHole);
+    }
+  };
+
+  const animationGirl = () => {
+    hole.style.animation = 'holeSmall 3s';
+    hole.style.width = '30%';
+    hole.style.height = '5%';
+    girl.style.bottom = '20px';
+    girl.style.animation = 'levitate 8s ease 0s infinite';
+    girl.removeEventListener('animationend', animationGirl);
+  };
+
+  hole.addEventListener('animationend', animationHole);
+  girl.addEventListener('animationend', animationGirl);
+})();
